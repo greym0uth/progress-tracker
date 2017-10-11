7f6288f7cc79d18b66beb651d0be28c2f1582214
Greymouth <thejadenjack@gmail.com>
2017-10-11 19:49:22 -0400

    Login calls

--- public-src/components/navigation/profile.vue
+++ public-src/components/navigation/profile.vue
@@ -1,25 +1,42 @@
 <template>
-	<v-menu bottom right :nudge-bottom="50" :nudge-width="100">
-		<v-btn color="primary" icon slot="activator">
+	<v-menu bottom right :nudge-bottom="50" :nudge-width="150">
+		<v-btn class="white--text" color="primary" icon slot="activator">
 			<v-icon>person</v-icon>
 		</v-btn>
-		<v-list>
+		<v-list v-if="userId">
 			<v-list-tile @click="">
 				<v-list-tile-title>
 					<v-layout row justify-space-between>
-						<v-flex xs1><v-icon>fingerprint</v-icon></v-flex>
-						<v-flex xs5><span>Login</span></v-flex>
+						<v-flex xs1><v-icon>account_circle</v-icon></v-flex>
+						<v-flex xs8><span>My Account</span></v-flex>
 					</v-layout>
 				</v-list-tile-title>
 			</v-list-tile>
+			<v-list-tile @click="">
+				<v-list-tile-title>
+					<v-layout row justify-space-between>
+						<v-flex xs1><v-icon>exit_to_app</v-icon></v-flex>
+						<v-flex xs5><span>Logout</span></v-flex>
+					</v-layout>
+				</v-list-tile-title>
+			</v-list-tile>
+		</v-list>
+		<v-list v-else>
+			<login-tile></login-tile>
 		</v-list>
 	</v-menu>
 </template>
 
 <script>
+import LoginComponent from './tiles/login.vue';
+
 export default {
+	props: ['userId'],
 	data: function() {
 		return {};
+	},
+	components: {
+		'login-tile': LoginComponent
 	}
 };
 </script>
---
+++ public-src/components/panels/login-panel.vue
@@ -0,0 +1,58 @@
+<template>
+	<v-card>
+		<v-card-title>
+			<span class="headline">Login</span>
+		</v-card-title>
+		<v-card-text>
+			<v-container grid-list-md>
+				<v-layout wrap>
+					<v-flex m8>
+						<v-text-field label="Username" v-model="username"></v-text-field>
+					</v-flex>
+					<v-flex m8>
+						<v-text-field label="Password" v-model="password"></v-text-field>
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</v-card-text>
+		<v-card-actions>
+			<v-spacer></v-spacer>
+			<v-btn color="blue darken-1" flat @click="signup">Sign Up</v-btn>
+			<v-btn color="blue darken-1" flat @click="submit">Login</v-btn>
+		</v-card-actions>
+	</v-card>
+</template>
+
+<script>
+import auth from '../../utils/auth.js';
+
+export default {
+	model: {
+		event: 'complete'
+	},
+	data: function() {
+		return {
+			username: '',
+			password: ''
+		};
+	},
+	methods: {
+		submit: function() {
+			const _this = this;
+
+			const user = {
+				username: this.username,
+				password: this.password
+			};
+
+			auth.login(user).then(function(res) {
+				console.log(res);
+				_this.$emit('complete', true);
+			});
+		},
+		signup: function() {
+
+		}
+	}
+};
+</script>
